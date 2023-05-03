5d3107bb92c91ad131afdd74932ca80411e228e0
IDragonThunderI <andrey.voltr@yandex.ru>
2023-05-03 09:56:29 +0400

    Добавьте файлы проекта.

---
+++ Task 1/Task1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+string toBinary(int value)
+{
+	string bin;
+	do {
+		bin += char('0' + value % 2);
+	} while (value >>= 1);
+	return string(bin.crbegin(), bin.crend());
+}
+
+int main()
+{
+	system("chcp 1251 > nul");
+	cout << "/* ЗАДАЧА №1 */\n\n";
+
+	int x;
+	vector<int> fibonacci(2, 1);
+	bool correctInput = false;
+	cout << "Введите количество элементов ряда Фибоначчи: ";
+
+	while (!correctInput)
+	{
+		try
+		{
+			if (!(cin >> x))
+				throw invalid_argument("Недопустимое значение параметра. Ожидалось значение типа unsigned int!");
+
+			if (x < 3)
+				throw invalid_argument("Количество элементов должно быть больше двух!");
+
+			correctInput = true;
+		}
+		catch (const invalid_argument& error)
+		{
+			cerr << "Error: " << error.what() << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+	}
+
+	for (size_t i = 2; i < x; i++)
+		fibonacci.push_back(fibonacci.at(i - 1) + fibonacci.at(i - 2));
+
+	for (size_t i = 0; i < fibonacci.size(); i++)
+	{
+		cout << setw(2) << left << i + 1 << " - dec: " << setw(5) << fibonacci.at(i);
+		cout << hex << "   |   hex: " << setw(5) << left << fibonacci.at(i); 
+		cout << oct << "   |   oct: " << setw(5) << left << fibonacci.at(i);
+		cout <<	"   |   bin: " << setw(5) << left << toBinary(fibonacci.at(i)) << endl;
+	}
+
+	return 0;
+}
---
+++ Task 2/Task2.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void fileCopy(ifstream& input, const string& outname)
+{
+	ofstream output;
+	output.open(outname);
+
+	if (!output.is_open())
+	{
+		cout << "Error: Ошибка при создании или открытии output файла!" << endl;
+		exit(3);
+	}
+	
+	string str;
+	while (getline(input, str))
+	{
+		for (size_t i = 0; i < str.length(); i++)
+			if (str.at(i) == ',') str.replace(i, 1, "");
+
+		output << str << endl;
+	}
+
+	output.close();
+}
+
+int main(int argc, char* argv[])
+{
+	system("chcp 1251 > nul");
+
+	string filename;
+
+	if (argc > 1) filename = argv[1];
+	else
+	{
+		cout << "Укажите файл для копирования: ";
+		getline(cin, filename);
+	}
+
+	ifstream input;
+	input.open(filename);
+
+	if (!input.is_open())
+	{
+		cout << "Error: Ошибка при открытии input файла!" << endl;
+		exit(2);
+	}
+
+	filename.erase(0, filename.find_last_of("/\\") + 1);
+	string outname;
+	if (argc > 2)
+	{
+		outname = argv[2];
+		if (outname.size() - 1 == outname.find_last_of("/\\"))
+			outname += "copy - " + filename;
+	}
+	else outname = "copy - " + filename;
+
+	fileCopy(input, outname);
+
+	input.close();
+	cout << "Файл успешно скопирован!\n";
+
+	return 0;
+}
